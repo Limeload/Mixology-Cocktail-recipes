dd038a56c4f594e002f5701797ecf61ccb6e3969
Shraddha Rao <shraddharaom@gmail.com>
2022-12-05 13:02:58 -0600

    displaying cocktail recipe in a card format

--- index.js
+++ index.js
@@ -8,7 +8,30 @@ function fetchDrinks() {
     fetch(url)
     .then((response => response.json()))
     .then(drinks =>  {
-        console.log(drinks)
+        displayDrinks(drinks)
 
     })
 }
+
+let cocktailList = document.querySelector("#cocktail-list");
+
+function displayDrinks (drinks) {
+ const card = drinks.map(drinks => {
+   return `
+   <div class="container-card">
+   <div id="card" style="width:18rem";>
+   <img src = "${drinks.strDrinkThumb}" class="card-img-top" alt="${drinks.strDrink}">
+   <div id="card-body">
+   </div>
+   </div>
+   </div>
+   `
+ })
+
+ cocktailList.innerHTML += card;
+}
+
+
+// Search my name - by Waltz
+// random cocktail - by amy
+// toggle darkmode - by waltz
