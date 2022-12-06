3049ef7acd468cb0d7f8c8ef9a7e6c01e29e158f
Shraddha <80426069+Limeload@users.noreply.github.com>
2022-12-05 18:58:46 -0600

    Revert "GET fetch using db.json"

--- index.js
+++ index.js
@@ -4,34 +4,4 @@ document.addEventListener('DOMContentLoaded', () => {
 
 // GET request
 function fetchDrinks() {
-    const url = "http://localhost:3000/drinks"
-    fetch(url)
-    .then((response => response.json()))
-    .then(drinks =>  {
-        displayDrinks(drinks)
-
-    })
 }
-
-let cocktailList = document.querySelector("#cocktail-list");
-
-function displayDrinks (drinks) {
- const card = drinks.map(drinks => {
-   return `
-   <div class="container-card">
-   <div id="card" style="width:18rem";>
-   <img src = "${drinks.strDrinkThumb}" class="card-img-top" alt="${drinks.strDrink}">
-   <div id="card-body">
-   </div>
-   </div>
-   </div>
-   `
- })
-
- cocktailList.innerHTML += card;
-}
-
-
-// Search my name - by Waltz
-// random cocktail - by amy
-// toggle darkmode - by waltz
